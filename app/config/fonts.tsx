823f520050998e836ab207875217eacab98c3c8c
hernandoabella <hernandoabella@gmail.com>
2026-01-18 23:45:36 -0500

    removing repeated stuff

--- app/config/fonts.tsx
+++
@@ -1,5 +0,0 @@
-const FONTS = [
-  { name: "Fira Code", family: '"Fira Code", monospace' },
-  { name: "JetBrains Mono", family: '"JetBrains Mono", monospace' },
-  { name: "Source Code", family: '"Source Code Pro", monospace' },
-];
